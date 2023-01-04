4a85795a9576d96d22d11dce84c4b3b563e3f67d
Michael <mbuttgereit@dev-10.com>
2023-01-04 09:14:10 -0600

    adding sql info

---
+++ Server/sql/Monster_Bash_ddl.sql
@@ -0,0 +1,50 @@
+drop database if exists monster_bash;
+create database monster_bash;
+use monster_bash;
+
+create table App_User (
+	user_id int auto_increment primary key,
+	username varchar(120) not null unique,
+    `password` varchar(2048) not null,
+    enabled bit not null 
+);
+
+create table App_Role (
+	role_id int auto_increment primary key,
+    role_name varchar(120) not null unique
+);
+
+create table App_Role_User (
+	user_id int not null,
+    role_id int not null,
+    
+    constraint foreign key (user_id) references App_User(user_id),
+    constraint foreign key (role_id) references App_Role(role_id),
+    constraint primary key (user_id, role_id)
+);
+
+insert into App_User (user_id, username, password, enabled) values (1, 'bmadrell0', 'MrvxYAfFx', false);
+insert into App_User (user_id, username, password, enabled) values (2, 'uomannion1', '619VkMuG', true);
+insert into App_User (user_id, username, password, enabled) values (3, 'rcollinwood2', 'JCHD514H7z8', false);
+insert into App_User (user_id, username, password, enabled) values (4, 'bbeange3', 'ETDxxA3Oj', false);
+insert into App_User (user_id, username, password, enabled) values (5, 'hsturzaker4', 'JrUyQhIrTh2', true);
+insert into App_User (user_id, username, password, enabled) values (6, 'dchave5', 'rWXAV8eQ', true);
+insert into App_User (user_id, username, password, enabled) values (7, 'sdhennin6', 'cx0Oi7', false);
+insert into App_User (user_id, username, password, enabled) values (8, 'orown7', 'ixliMEd2yUE6', false);
+insert into App_User (user_id, username, password, enabled) values (9, 'bkellie8', 'XlIXJAg8', true);
+insert into App_User (user_id, username, password, enabled) values (10, 'rbart9', 'Tz7r9Ba18', true);
+
+insert into App_Role (role_id, role) values (1, admin);
+insert into App_Role (role_id, role) values (2, `user`);
+
+insert into App_Role_User(role_id, user_id) values (1,1);
+insert into App_Role_User(role_id, user_id) values (2,2);
+insert into App_Role_User(role_id, user_id) values (2,3);
+insert into App_Role_User(role_id, user_id) values (2,4);
+insert into App_Role_User(role_id, user_id) values (2,5);
+insert into App_Role_User(role_id, user_id) values (2,6);
+insert into App_Role_User(role_id, user_id) values (2,7);
+insert into App_Role_User(role_id, user_id) values (2,8);
+insert into App_Role_User(role_id, user_id) values (2,9);
+insert into App_Role_User(role_id, user_id) values (2,10);
+
