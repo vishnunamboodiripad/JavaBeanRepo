69df9a6e2ad3b87edd3b2bc8e93000715251ebfc
vishnunamboodiripad <115960786+vishnunamboodiripad@users.noreply.github.com>
2023-01-09 09:09:28 -0600

    affinity

--- Server/src/main/java/learn/monsterBash/data/AffinityJdbcTemplateRepository.java
+++ Server/src/main/java/learn/monsterBash/data/AffinityJdbcTemplateRepository.java
@@ -1,4 +1,94 @@
 package learn.monsterBash.data;
 
+import learn.monsterBash.data.mappers.AffinityMapper;
+import learn.monsterBash.data.mappers.EquipmentMapper;
+import learn.monsterBash.data.mappers.WeatherMapper;
+import learn.monsterBash.models.Affinity;
+import learn.monsterBash.models.Equipment;
+import learn.monsterBash.models.Weather;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.List;
+
 public class AffinityJdbcTemplateRepository implements AffinityRepo {
-}
+    private final JdbcTemplate jdbcTemplate;
+
+    public AffinityJdbcTemplateRepository(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;}
+    @Override
+    public List<Affinity> findAll() {
+
+            final String sql = """
+                    select affinity_id, affinity_name
+                    from affinity
+                    order by affinity_id;
+                    """;
+
+            return jdbcTemplate.query(sql, new AffinityMapper());
+
+    }
+
+    @Override
+    public Affinity add(Affinity affinity) {
+
+        final String sql = """
+                insert into affinity (affinity_name)
+                values (?);
+                 """;
+
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        int rowsAffected = jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, affinity.getAffinityName());
+            return ps;
+        }, keyHolder);
+
+        if (rowsAffected <= 0) {
+            return null;
+        }
+
+        affinity.setAffinityId(keyHolder.getKey().intValue());
+        return affinity;
+    }
+
+    @Override
+    @Transactional
+    public Affinity findById(int affinityId) {
+
+        final String sql = """
+               select affinity_id, affinity_name
+               from affinity
+               where affinity_id = ?;
+               """;
+
+        Affinity affinity = jdbcTemplate.query(sql, new AffinityMapper(), affinityId).stream()
+                .findFirst().orElse(null);
+
+        return affinity;
+    }
+
+    @Override
+    public boolean update(Affinity affinity) {
+        final String sql = """
+                update affinity set
+                affinity_name = ?,
+                where affinity_id = ?
+                """;
+
+        return jdbcTemplate.update(sql,
+                affinity.getAffinityName(),
+                affinity.getAffinityId()) > 0;
+    }
+
+
+    @Override
+    public boolean deleteById(int affinityId) {
+            return jdbcTemplate.update("delete from equipment where affinity_id = ?;", affinityId) > 0;
+        }
+    }
+
--- Server/src/main/java/learn/monsterBash/domain/AffinityService.java
+++ Server/src/main/java/learn/monsterBash/domain/AffinityService.java
@@ -1,4 +1,99 @@
 package learn.monsterBash.domain;
 
+import learn.monsterBash.data.AffinityRepo;
+import learn.monsterBash.data.WeatherRepo;
+import learn.monsterBash.models.Affinity;
+import learn.monsterBash.models.Weather;
+
+import java.util.List;
+import java.util.Optional;
+
 public class AffinityService {
+    private final AffinityRepo repo;
+
+
+    public AffinityService(AffinityRepo repo) {
+        this.repo = repo;
+    }
+
+    public List<Affinity> findAll() {
+        return repo.findAll();
+    }
+
+    public Affinity findByID(int affinityId){
+        return repo.findById(affinityId);
+    }
+
+    public Result<Affinity> add(Affinity affinity){
+        Result<Affinity> result = validate(affinity);
+        if (!result.isSuccess()){
+            return result;
+        }
+
+        if(affinity.getAffinityId() !=0) {
+            result.addMessage("affinityId should not be set by user", ResultType.INVALID);
+            return result;
+        }
+        List<Affinity> affinities = repo.findAll();
+        String name = affinity.getAffinityName();
+        Optional<Affinity> duplicate = affinities.stream().filter(w -> w.getAffinityName().equalsIgnoreCase(name)).findAny();
+        if (duplicate.isPresent()) {
+            result.addMessage("Affinity name must be unique", ResultType.INVALID);
+            return result;
+        }
+        affinity = repo.add(affinity);
+        result.setPayload(affinity);
+        return result;
+    }
+
+    public Result<Affinity> update(Affinity affinity) {
+        Result<Affinity> result = validate(affinity);
+        if (!result.isSuccess()) {
+            return result;
+        }
+        if (affinity.getAffinityId() <= 0) {
+            result.addMessage("affinityId was not correctly set by user", ResultType.INVALID);
+            return result;
+        }
+        List<Affinity> affinities = repo.findAll();
+        String name = affinity.getAffinityName();
+        Optional<Affinity> duplicateName = affinities.stream().filter(w -> w.getAffinityName().equalsIgnoreCase(name)).findAny();
+        if (duplicateName.isPresent()) {
+            result.addMessage("That affinity name already exists", ResultType.INVALID);
+            return result;
+        }
+
+        boolean update = repo.update(affinity);
+        if (!update) {
+            String message = String.format("Affinity with Id: %s was not found", affinity.getAffinityId());
+            result.addMessage(message, ResultType.NOT_FOUND);
+        }
+
+        return result;
+
+    }
+
+    public Result<Affinity> deleteById(int affinityId){
+        Result<Affinity> result = new Result<>();
+        boolean delete = repo.deleteById(affinityId);
+        if (!delete) {
+            String message = String.format("Affinity with Id: %s was not found", affinityId);
+            result.addMessage(message, ResultType.NOT_FOUND);
+        }
+        return result;
+    }
+
+    private Result<Affinity> validate(Affinity affinity) {
+        Result<Affinity> result = new Result<>();
+        if (affinity == null) {
+            result.addMessage("Cannot add a null weather", ResultType.INVALID);
+            return result;
+        }
+        if (affinity.getAffinityName().isBlank() || affinity.getAffinityName() == null) {
+            result.addMessage("Cannot add weather without a name", ResultType.INVALID);
+            return result;
+        }
+
+        return result;
+    }
 }
