11fe4eb18acaa02551cc3f6028c39a5ee755501c
vishnunamboodiripad <115960786+vishnunamboodiripad@users.noreply.github.com>
2023-01-09 14:41:38 -0600

    amending problems

--- Server/src/main/java/learn/monsterBash/models/Battle.java
+++ Server/src/main/java/learn/monsterBash/models/Battle.java
@@ -4,13 +4,13 @@ public class Battle {
 
     private int battleId;
 
-    private Monster monster1;
+    private Monster playerMonster;
 
-    private Monster monster2;
+    private Monster computerMonster;
 
-    private Equipment equipment1;
+    private Equipment playerEquipment;
 
-    private Equipment equipment2;
+    private Equipment computerEquipment;
 
     private Weather weather;
 
@@ -18,7 +18,89 @@ public class Battle {
 
     private String result;
 
-    private AppUser user;
+    private int AppUser;
+
+    private Boolean playerWin;
+
+    public int getBattleId() {
+        return battleId;
+    }
+
+    public void setBattleId(int battleId) {
+        this.battleId = battleId;
+    }
+
+    public Monster getPlayerMonster() {
+        return playerMonster;
+    }
+
+    public void setPlayerMonster(Monster playerMonster) {
+        this.playerMonster = playerMonster;
+    }
+
+    public Monster getComputerMonster() {
+        return computerMonster;
+    }
+
+    public void setComputerMonster(Monster computerMonster) {
+        this.computerMonster = computerMonster;
+    }
+
+    public Equipment getPlayerEquipment() {
+        return playerEquipment;
+    }
+
+    public void setPlayerEquipment(Equipment playerEquipment) {
+        this.playerEquipment = playerEquipment;
+    }
+
+    public Equipment getComputerEquipment() {
+        return computerEquipment;
+    }
+
+    public void setComputerEquipment(Equipment computerEquipment) {
+        this.computerEquipment = computerEquipment;
+    }
+
+    public Weather getWeather() {
+        return weather;
+    }
+
+    public void setWeather(Weather weather) {
+        this.weather = weather;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    public int getAppUser() {
+        return AppUser;
+    }
+
+    public void setAppUser(int appUser) {
+        AppUser = appUser;
+    }
+
+    public Boolean getPlayerWin() {
+        return playerWin;
+    }
+
+    public void setPlayerWin(Boolean playerWin) {
+        this.playerWin = playerWin;
+    }
 
     public Battle() {
     }
